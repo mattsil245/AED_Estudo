1fd459896be67ac5e8f939cef812f809c4a5ea2a
mattsil245 <moliveiradasilva494@gmail.com>
2025-10-21 12:34:31 -0300

    Lista sequencial dinâmica comentada

--- Listas_Sequenciais/listas/lista_sequencial_dinamica.c
+++ Listas_Sequenciais/listas/lista_sequencial_dinamica.c
@@ -1,29 +1,37 @@
+//Mesma coisa de sempre
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "lista_sequencial_dinamica.h"
 
+//Atenção, funçao mudou, agora recebe uma capacidade = tamanho = n
 ListaSequencial * cria_lista(int capacidade){
 
+	//Ponteiro p/lista no caso a strcut, estou reservando um espaco na memorial para ela
 	ListaSequencial * lista =  (ListaSequencial *) malloc (sizeof(ListaSequencial));
+	//vetor a = vetor de elementos(int*) Malloc(tamanho + 1?) * tamanho de um elemento(int) 
 	lista->a = (Elemento *) malloc ((capacidade + 1) * sizeof(Elemento));
-	lista->capacidade = capacidade;
-	lista->livre = 0;
+	lista->capacidade = capacidade; //tamanho = capacidade = n
+	lista->livre = 0; //Inicializa com 0 a primeira posicao a ser ocupada
 
+	//retorna o ponteiro para a lista
 	return lista;
 }
 
+//Mudancas, primeiro destruo o vetor da lista, depois a lista, destruir = liberar da memoria
 void destroi_lista(ListaSequencial * lista){
 
 	free(lista->a);
 	free(lista);
 }
 
+//Mesma coisa
 int tamanho(ListaSequencial * lista){
 
 	return lista->livre;
 }
 
+//Mesma coisa
 void imprime(ListaSequencial * lista){
 
 	int i;
@@ -38,17 +46,23 @@ void imprime(ListaSequencial * lista){
 	printf("\n");
 }
 
+//Uso da sentinela - por isso capacidade + 1
 int busca(ListaSequencial * lista, Elemento e){
 
+	//incializa o contador
 	int i;
 
-	lista->a[lista->livre] = e;
+	//sempre vai ter um espaco livre ja que capacidade = capacidade mas tamanho da lista = capacidade +1
+	lista->a[lista->livre] = e; //a posicao vaga depois do ultimo valor a ser inserido recebe o valor que eu quero achar
 
-	for(i = 0; lista->a[i] != e; i++);
+	//nao precisamos mais do if, logo, menos iterações = menor custo
+	for(i = 0; lista->a[i] != e; i++); //vai rodando ate nao der mais, pior caso = final da lista/livre + 1 (onde esta a sentinela)
 
+	//caso o indice seja menor que livre = achamos, caso não, ele esta em livre - 1, onde colocamos a sentinela, logo, o elemento não existe na lista
 	return (i < lista->livre) ? i : -1;
 }
 
+//Mudancas referentes a substituicao da constante no condicional por um atributo da lista que guarda o tamanho maximo da mesma
 Boolean insere(ListaSequencial * lista, Elemento e, int indice){
 
 	int i;
@@ -69,6 +83,7 @@ Boolean insere(ListaSequencial * lista, Elemento e, int indice){
 	return FALSE;
 }
 
+//Mesma coisa
 Boolean remove_elemento(ListaSequencial * lista, Elemento e){
 
 	int i;
--- Listas_Sequenciais/listas/lista_sequencial_dinamica.h
+++ Listas_Sequenciais/listas/lista_sequencial_dinamica.h
@@ -1,18 +1,24 @@
+//Constantes de lógica mantidas, porém constante da tamanho foi removida
 #define FALSE 0
 #define TRUE 1
 
+//Mesma coisa as declarações
 typedef int Boolean;
 typedef int Elemento;
 
 typedef struct {
-
+	//vetor agora nao tem tamanho fixo, agora é so um ponteiro, array de inteiros
 	Elemento * a;
+	//Continua igual
 	int livre;
+	//Nova variável para guardar o tamanho do array dinamicamente
 	int capacidade;
 
 } ListaSequencial;
 
+//Agora criar lista recebe um parametro de tamanho n = capacidade = tamanho do array
 ListaSequencial * cria_lista(int n);
+//O resto continua igual, pelo menos na declaração e parametros
 void destroi_lista(ListaSequencial * lista);
 int tamanho(ListaSequencial * lista);
 void imprime(ListaSequencial * lista);
